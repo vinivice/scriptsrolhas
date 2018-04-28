f66b37035513e09e0f683bf5b02d69d6059df2ee
Vinicius <vinivice@gmail.com>
2018-04-28 04:56:50 -0300

    improved dlcp and created dlacp

---
+++ download_all_chapters.sh
@@ -0,0 +1,15 @@
+####################################################################
+#            Used to download the whole series of a manga          #
+#    Created to be used in specific sites. May not work on others  #
+#      Use the scrip download_chapter.sh. Make sure you have it    #
+#               Pass the url of manga as an argument               #
+#   It doesn't check for wrong input, the download will only fail  #
+# Only download integer chapter. Does not account for special ones #
+####################################################################
+
+CHAPTER=1
+DONE=0
+
+while [ "$DONE" == "0" ]; do
+
+done
--- download_chapter.sh
+++ download_chapter.sh
@@ -1,11 +1,21 @@
+################################################################
+#               Used to download manga chapters                #
+#   Created to use in specific sites. May not work on others   #
+# Use the url of the first page as the parameter to the script #
+#  It does not treat wrong inputs, the download will only fail #
+################################################################
 PAGE=1
 DONE=0
+LOCAL_PATH=$(pwd)
 
 if [ "$#" -ne 1 ]; then
 	echo Error: Must pass the url of the first page as argument
+	echo USage: ./$0 URL
 	exit 1
 fi
 
+cd /tmp
+rm -rf target
 #echo 0
 #exit 0
 echo Starting download
@@ -13,16 +23,22 @@ while [ "$DONE" == "0" ]; do
 	wget -q -x -H -r -l 1 -A jpg,jpeg -P target $1/$PAGE
 
 	if [ -d "target" ]; then
-		rm -r target/www*
-		rm -r target/*.net
+		rm -rf target/www*
+		rm -rf target/*.net
 
-		cd target/*/*/*
-		mv * $PAGE.jpg
-		cd - > /dev/null
-		rsync -r target/*/* mangas
-		rm -r target
-		echo Page $PAGE downloaded
-		PAGE=$((PAGE+1))
+		cd target/*/*/* 2> /dev/null
+		if [ "$?" == "0" ]; then
+			mv * $PAGE.jpg
+			cd - > /dev/null
+			rsync -r target/*/* $LOCAL_PATH/mangas
+			rm -rf target
+			echo Page $PAGE downloaded
+			PAGE=$((PAGE+1))
+		else
+			echo Error: Probably inexistent chapter.
+			rm -rf target
+			exit 5
+		fi
 	else
 		DONE=1
 	fi
@@ -32,4 +48,6 @@ while [ "$DONE" == "0" ]; do
 	#sleep 10
 done
 
+rm -rf target
 echo Download finished
+exit 0
